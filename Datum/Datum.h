261fa0b4c9cc052d9098ebbbe21183d924d685c0
Aron Cedercrantz <aron@cedercrantz.se>
2013-09-03 19:58:14 +0200

    Add shell project.

---
+++ Datum/Datum.h
@@ -0,0 +1,9 @@
+//
+//  Datum.h
+//  Datum
+//
+//  Created by Aron Cedercrantz on 03-09-2013.
+//  Copyright (c) 2013 Aron Cedercrantz. All rights reserved.
+//
+
+
---
+++ DatumTests/DatumTests.m
@@ -0,0 +1,34 @@
+//
+//  DatumTests.m
+//  DatumTests
+//
+//  Created by Aron Cedercrantz on 03-09-2013.
+//  Copyright (c) 2013 Aron Cedercrantz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DatumTests : XCTestCase
+
+@end
+
+@implementation DatumTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
