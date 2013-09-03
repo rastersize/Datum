b868488ab3288df67dd22ffa67f7f84b117d95b4
Aron Cedercrantz <aron@cedercrantz.se>
2013-09-03 20:49:12 +0200

    Add DATObject.
    - Does not actually contain anything.

---
+++ Datum/DATObject.h
@@ -0,0 +1,13 @@
+//
+//  DATObject.h
+//  Datum
+//
+//  Created by Aron Cedercrantz on 03-09-2013.
+//  Copyright (c) 2013 Aron Cedercrantz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DATObject : NSObject
+
+@end
---
+++ Datum/DATObject.m
@@ -0,0 +1,13 @@
+//
+//  DATObject.m
+//  Datum
+//
+//  Created by Aron Cedercrantz on 03-09-2013.
+//  Copyright (c) 2013 Aron Cedercrantz. All rights reserved.
+//
+
+#import "DATObject.h"
+
+@implementation DATObject
+
+@end
